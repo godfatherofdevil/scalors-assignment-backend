4592741be451b086abf1aca46be297c6d047c581
Avinash Kumar <akumar@healthprecision.com>
2020-11-20 10:21:15 +0200

    added board and todo models.

--- clinomics/todo/models.py
+++ clinomics/todo/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class Board(models.Model):
+    name = models.CharField(max_length=100)
+    owner = models.ForeignKey("auth.User", related_name="board_owner", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.id}:{self.name}"
+
+    def __unicode__(self):
+        return self.name
+
+
+class Todo(models.Model):
+    title = models.CharField(max_length=100)
+    done = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    board = models.ForeignKey(Board, on_delete=models.CASCADE, related_name="todos")
+    owner = models.ForeignKey("auth.User", related_name="todo_owner", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.board}:{self.title}"
+
+    def __unicode__(self):
+        return f"{self.title}: {self.created}"
+
+    class Meta:
+        ordering = ["created", ]
