fc8d676c5d5b2ae4280301910ba49f93b96de66e
Avinash Kumar <akumar@healthprecision.com>
2020-11-20 14:35:18 +0200

    remove secret key.

--- clinomics/clinomics/settings.py
+++ clinomics/clinomics/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ykqc)i%99tj0$&bg7n3w86!z-f40m4lfdbw3ewkibs&c$d$1m-'
+SECRET_KEY = None
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
